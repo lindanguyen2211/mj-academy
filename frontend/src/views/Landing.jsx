f09bc9bbeca9150677d4227257c4eb2c678f5a6f
Linda Nguyen <209385033+lindanguyen2211@users.noreply.github.com>
2025-03-19 14:54:46 -0500

    feat(landing): add temporary links to landing

--- frontend/src/views/Landing.jsx
+++ frontend/src/views/Landing.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import "./Landing.css";
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
     <div>
-      Landing
+      <ul>
+        <li>
+          <Link to="/dashboard">User Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/courses">Getting Started</Link>
+          </li>
+        <li>
+          <Link to="/courses/exam">Exam</Link>
+        </li>
+        <li>
+          <Link path="/certificates">Certificates</Link>
+        </li>
+        <li>
+          <Link path="/library">Library</Link>
+        </li>
+      </ul>
     </div>
   );
 };
