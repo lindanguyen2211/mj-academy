5c5aad88b5c1bf1901cb8aeb04a7540458373584
Linda Nguyen <209385033+lindanguyen2211@users.noreply.github.com>
2025-03-19 17:09:29 -0500

    feat(user-dashboard): add progress to data set

--- frontend/src/views/UserDashboard.jsx
+++ frontend/src/views/UserDashboard.jsx
@@ -10,33 +10,37 @@ const UserDashboard = () => {
       courseTitle: "Getting Started",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
-        img: new URL("../assets/images/MODJAW_Avatar-screen-1.png", import.meta.url).href
+        img: new URL("../assets/images/MODJAW_Avatar-screen-1.png", import.meta.url).href,
+        progress: 100
       },
     {
       id: 2,
       courseTitle: "Introduction to 4D Dentistry",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
-        img: new URL("../assets/images/Modjaw-aesthetic-light-facescan-1.png", import.meta.url).href
+        img: new URL("../assets/images/Modjaw-aesthetic-light-facescan-1.png", import.meta.url).href,
+        progress: 50
       },
     {
       id: 3,
       courseTitle: "Clinical Training",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
-        img: new URL("../assets/images/MODJAW_Twim-registration-record-1.png", import.meta.url).href
+        img: new URL("../assets/images/MODJAW_Twim-registration-record-1.png", import.meta.url).href,
+        progress: 0
       },
     {
       id: 4,
       courseTitle: "Lab Training",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
-        img: new URL("../assets/images/Modjaw-Balkwill-TWIM-1.png", import.meta.url).href
+        img: new URL("../assets/images/Modjaw-Balkwill-TWIM-1.png", import.meta.url).href,
+        progress: 0
       },
   ];
 
   return (
-    <section id="user-dashboard">
+    <section id='user-dashboard' className='container'>
       <h1>Welcome to MODJAW&trade; Digital Academy</h1>
       <h2>Introducing 4D Dentistry to Digital Workflows</h2>
 
@@ -46,6 +50,7 @@ const UserDashboard = () => {
           title={item.courseTitle}
           description={item.description}
           img={item.img}
+          progress={item.progress}
         />
       ))}
     </section>
