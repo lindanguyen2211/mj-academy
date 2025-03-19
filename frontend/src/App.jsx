05f37f226512e338804c5fa751ae17e976b8561e
Linda Nguyen <209385033+lindanguyen2211@users.noreply.github.com>
2025-03-19 14:30:06 -0500

    feat(routing): link routes to views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import Landing from './views/Landing'
+import UserDashboard from './views/UserDashboard'
+import CourseDashboard from './views/CourseDashboard'
+import Exam from './views/Exam'
+import Certificates from './views/Certificates'
+import Library from './views/Library'
 
 function App() {
 
   return (
     <>
       <Routes>
-        {/* !TODO - Add <Route> for each view */}
-        <Route />
+        <Route path="/" element={<Landing/>} />
+        <Route path="/dashboard" element={<UserDashboard/>} />
+        {/* !TODO course dashboard changes dynamically based on course selected from user dashboard  
+        
+          path='/courses/:id'
+        */}
+        <Route path='/courses' element={<CourseDashboard />} />
+        {/* exam correspond to specific course  */}
+        <Route path='/courses/exam' element={<Exam />} />
+        <Route path='/certificates' element={<Certificates />} /> 
+        <Route path='/library' element={<Library />} /> 
       </Routes>
 
     </>
