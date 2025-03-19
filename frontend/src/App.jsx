f209d342581b3e6939e188939db5479e8c0725e7
Linda Nguyen <209385033+lindanguyen2211@users.noreply.github.com>
2025-03-19 13:56:27 -0500

    feat(routing): setup for routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <Routes>
+        {/* !TODO - Add <Route> for each view */}
+        <Route />
+      </Routes>
 
     </>
   )
