e9be53553e7606bdb247c9b3b1173a78ec7acb92
Linda Nguyen <209385033+lindanguyen2211@users.noreply.github.com>
2025-03-19 16:03:46 -0500

    feat(user-dashboard): placeholder data for courses

--- frontend/src/views/UserDashboard.jsx
+++ frontend/src/views/UserDashboard.jsx
@@ -2,6 +2,38 @@ import React from 'react'
 import './UserDashboard.css'
 
 const UserDashboard = () => {
+  // placeholder data for courses, setup to retrieve from database later
+  const courses = [
+    {
+      id: 1,
+      courseTitle: "Getting Started",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
+      img: "/assets/images/MODJAW_Avatar-screen 1.png",
+    },
+    {
+      id: 2,
+      courseTitle: "Introduction to 4D Dentistry",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
+      img: "/assets/images/Modjaw-aesthetic-light-facescan 1.png",
+    },
+    {
+      id: 3,
+      courseTitle: "Clinical Training",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
+      img: "/assets/images/MODJAW_Twim-registration-record 1.png",
+    },
+    {
+      id: 4,
+      courseTitle: "Lab Training",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut aspernatur quasi excepturi libero veritatis atque id? Et harum cumque error.",
+      img: "/assets/images/Modjaw-Balkwill-TWIM 1.png",
+    },
+  ];
+
   return (
     <div>UserDashboard</div>
   )
