28f27f22b870b9cc22e43a7bf6b9196e6079b35c
Linda Nguyen <209385033+lindanguyen2211@users.noreply.github.com>
2025-04-19 17:46:20 -0500

    styling: color is conditional rendered based on progress of completed lessons

--- frontend/src/components/ProgressBar.jsx
+++ frontend/src/components/ProgressBar.jsx
@@ -6,7 +6,12 @@ const ProgressBar = ({ progress }) => {
     <div className="progressBar">
       <div 
         className="progressFill" 
-        style={{ width: `${progress}%` }}
+        style={ 
+          { width: `${progress}%`, backgroundColor: `${
+            progress < 100 ? "#ecdf6a" : "#4CAF50" }`
+          }
+          
+        }
       >
       </div>
     </div>
